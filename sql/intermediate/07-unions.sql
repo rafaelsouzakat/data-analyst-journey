6b7f4bf33ed55ef63284ec75f1089e1db4175e89
Rafael Souza K <rafahira55@gmail.com>
2026-08-16 21:59:28 +0000

    add UNIONS practice

---
+++ sql/intermediate/07-unions.sql
@@ -0,0 +1,30 @@
+-- Unions
+-- allows us to combine rows together
+-- we use a SELECT statement and use a UNION to combine with another SELECT statement
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION DISTINCT					# the UNION is DISTINCT by default
+SELECT first_name, last_name
+FROM employee_salary;
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL					
+SELECT first_name, last_name
+FROM employee_salary;
+
+-- Use case
+-- having multiple SELECT statements all combined using UNIONS
+SELECT first_name, last_name, 'Old Man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'Old Lady' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Highly Paid Employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name ASC;
