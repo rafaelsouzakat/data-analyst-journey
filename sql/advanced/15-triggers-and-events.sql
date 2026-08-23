e33d6fd418ead5df1bff59454f23cc8983a35a90
Rafael Souza K <rafahira55@gmail.com>
2026-08-23 23:02:23 +0000

    add triggers and events practice

---
+++ sql/advanced/15-triggers-and-events.sql
@@ -0,0 +1,43 @@
+-- Triggers and Events
+
+SELECT *
+FROM employee_demographics;
+
+SELECT *
+FROM employee_salary;
+
+DELIMITER $$
+CREATE TRIGGER employee_insert
+	AFTER INSERT ON employee_salary
+    FOR EACH ROW
+BEGIN
+	INSERT INTO employee_demographics (employee_id, first_name, last_name)
+    VALUES (NEW.employee_id, NEW.first_name, NEW.last_name);
+END $$
+DELIMITER ;
+
+
+INSERT INTO employee_salary (employee_id, first_name, last_name, occupation, salary, dept_id)
+VALUES (13 , 'Jean-Ralphio', 'Sapaerstein', 'Entertainment 720 CEO', 1000000, NULL);  	
+
+
+
+-- EVENTS
+# takes place when it's scheduled
+# it's very helpful for automation
+
+SELECT *
+FROM employee_demographics;
+
+DELIMITER $$
+CREATE EVENT delete_retirees
+ON SCHEDULE EVERY 30 SECOND
+DO 
+BEGIN
+	DELETE
+	FROM employee_demographics
+    WHERE age >= 60;
+END $$
+DELIMITER ;
+
+SHOW VARIABLES LIKE 'event%';
